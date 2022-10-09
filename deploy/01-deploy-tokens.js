968f7fddb3e766ced11240f8853abc49ef8fdd88
Boom <boom@Shubhams-MacBook-Pro.local>
2022-10-09 11:22:29 +0200

    added deployment script for contracts

---
+++ deploy/01-deploy-tokens.js
@@ -0,0 +1,17 @@
+module.exports = async (hre) => {
+    const {getNamedAccounts, deployments} = hre;
+    const {deploy} = deployments;
+    const {deployer} = await getNamedAccounts();
+
+    console.log("----------------------------")
+    
+    await deploy('RectangleToken', {
+      from: deployer,
+      args: [],
+      log: true,
+    });
+
+
+};
+
+module.exports.tags = ['RectangleToken'];
